802b1682a85ed1815b4b84ded381deffbadde09e
Alexandros Marinos <123497074+alexmarinos87@users.noreply.github.com>
2025-07-07 11:04:01 +0000

    [LeetCode Sync] Runtime - 1344 ms (30.44%), Memory - 0.0B (100.00%)

--- 1153-product-sales-analysis-i/solution.sql
+++ 1153-product-sales-analysis-i/solution.sql
@@ -1,9 +1,10 @@
-# Write your MySQL query statement below
 SELECT
     p.product_name,
-    s.year,
-    s.price
-FROM Sales AS s
-LEFT JOIN Product AS p ON
+    s.price,
+    s.year
+FROM
+    Sales AS s 
+LEFT JOIN
+    Product AS p
+ON
     s.product_id = p.product_id;
-    
