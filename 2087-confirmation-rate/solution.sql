da12aa528cab32f799f850cdf2e65d6562002acb
Alexandros Marinos <123497074+alexmarinos87@users.noreply.github.com>
2025-07-07 15:26:13 +0000

    [LeetCode Sync] Runtime - 615 ms (79.62%), Memory - 0.0B (100.00%)

--- 2087-confirmation-rate/solution.sql
+++ 2087-confirmation-rate/solution.sql
@@ -1,18 +1,18 @@
 SELECT
     s.user_id,
-    -- count how many “confirmed” rows,
-    -- divide by total rows, null‐safe, default to 0
-    ROUND(COALESCE(
-      SUM(CASE WHEN c.action = 'confirmed' THEN 1 ELSE 0 END)
-      * 1.0
-      / NULLIF(COUNT(c.action), 0)
-    , 0),2) AS confirmation_rate
-FROM
+    ROUND(
+        AVG(
+            CASE WHEN c.action = 'confirmed'
+                THEN 1 
+                    ELSE 0 
+                END)
+            ,2)
+        AS confirmation_rate 
+FROM 
     Signups AS s
-LEFT JOIN
+LEFT JOIN 
     Confirmations AS c
-  ON
+ON 
     s.user_id = c.user_id
-GROUP BY
+GROUP BY 
     s.user_id;
-
