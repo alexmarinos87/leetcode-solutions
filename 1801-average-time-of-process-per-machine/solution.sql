8f6a5e0fdc32e38f8682c94ba246372cec2899e5
Alexandros Marinos <123497074+alexmarinos87@users.noreply.github.com>
2025-07-08 13:29:38 +0000

    [LeetCode Sync] Runtime - 240 ms (59.27%), Memory - 0.0B (100.00%)

--- 1801-average-time-of-process-per-machine/solution.sql
+++ 1801-average-time-of-process-per-machine/solution.sql
@@ -1,18 +1,15 @@
 WITH CTE AS(
-SELECT
+    SELECT
         machine_id,
         process_id,
-        MIN(CASE WHEN activity_type = 'Start' THEN `timestamp` END) AS start_time,
-        MAX(CASE WHEN activity_type = 'End' THEN `timestamp` END) AS end_time
-    FROM 
-        Activity
-    GROUP BY 
-        machine_id, process_id
+        MIN(CASE WHEN activity_type = 'start' THEN `timestamp` END) AS start_time,
+        MAX(CASE WHEN activity_type = 'end' THEN `timestamp` END) AS end_time
+    FROM Activity
+    GROUP BY machine_id, process_id
 )
 SELECT 
     machine_id,
-    ROUND(AVG(end_time - start_time ),3) AS processing_time
+    ROUND(AVG(end_time - start_time),3) AS processing_time
 FROM
     CTE
-GROUP BY
-    machine_id;
+GROUP BY machine_id;
