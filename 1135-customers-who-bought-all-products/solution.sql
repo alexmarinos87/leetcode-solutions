96256728635ff15fce76d7869d4f1ab130203010
Alexandros Marinos <123497074+alexmarinos87@users.noreply.github.com>
2025-07-12 10:34:39 +0000

    [LeetCode Sync] Runtime - 512 ms (91.02%), Memory - 0.0B (100.00%)

---
+++ 1135-customers-who-bought-all-products/solution.sql
@@ -0,0 +1,6 @@
+select a.customer_id
+from (
+    select c.customer_id, count(distinct(c.product_key)) from customer as c
+    group by c.customer_id
+    having count(distinct(c.product_key)) = (select count(product_key) from product)
+) as a;
