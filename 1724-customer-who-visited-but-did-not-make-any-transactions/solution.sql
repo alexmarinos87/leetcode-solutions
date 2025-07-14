d045bd3ad74dfa919dfb6d2d81f10f374986bfce
Alexandros Marinos <123497074+alexmarinos87@users.noreply.github.com>
2025-07-14 13:12:53 +0000

    [LeetCode Sync] Runtime - 496 ms (62.75%), Memory - 0.0B (100.00%)

--- 1724-customer-who-visited-but-did-not-make-any-transactions/solution.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/solution.sql
@@ -1,13 +1,6 @@
-SELECT
-    v.customer_id,
+SELECT DISTINCT v.customer_id,
     COUNT(v.visit_id) AS count_no_trans
-FROM
-    Visits AS v
-LEFT JOIN
-    Transactions AS t
-ON
-    v.visit_id = t.visit_id
-WHERE
-    t.transaction_id IS NULL
-GROUP BY
-    v.customer_id;
+FROM   Visits AS v
+LEFT   JOIN Transactions AS t ON v.visit_id = t.visit_id
+WHERE  t.visit_id IS NULL
+GROUP BY v.customer_id;
