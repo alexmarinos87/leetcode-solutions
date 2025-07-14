5f42ec9e00feaf98325428434f66e5580f4fac4f
Alexandros Marinos <123497074+alexmarinos87@users.noreply.github.com>
2025-07-14 13:24:19 +0000

    [LeetCode Sync] Runtime - 400 ms (13.76%), Memory - 0.0B (100.00%)

--- 0570-managers-with-at-least-5-direct-reports/solution.sql
+++ 0570-managers-with-at-least-5-direct-reports/solution.sql
@@ -1,10 +1,17 @@
-SELECT m.name
-FROM Employee AS m
-JOIN (
-   SELECT managerId
-   FROM Employee
-   GROUP BY managerId
-   HAVING COUNT(*) >= 5
-) AS grp
-  ON m.id = grp.managerId;
-
+-- Step 1: find managers with ≥5 reports
+WITH mgr_counts AS (
+  SELECT
+    managerId,
+    COUNT(*) AS cnt_reports
+  FROM Employee
+  WHERE managerId IS NOT NULL
+  GROUP BY managerId
+  HAVING COUNT(*) >= 5
+)
+-- Step 2: join back to get manager names
+SELECT
+  e.name
+FROM
+  mgr_counts AS mc
+  JOIN Employee AS e
+    ON e.id = mc.managerId;
