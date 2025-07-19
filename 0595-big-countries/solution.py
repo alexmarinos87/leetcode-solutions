2d80a040da959fad1996a84cadf63545416b5a04
Alexandros Marinos <123497074+alexmarinos87@users.noreply.github.com>
2025-07-19 21:54:09 +0000

    [LeetCode Sync] Runtime - 340 ms (5.24%), Memory - 68.1 MB (97.98%)

---
+++ 0595-big-countries/solution.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+def big_countries(world: pd.DataFrame) -> pd.DataFrame:
+    mask = (world['area'] >= 3_000_000) | (world['population'] >= 25_000_000)
+    return world.loc[mask, ['name', 'population', 'area']]
