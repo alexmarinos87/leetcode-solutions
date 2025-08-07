0e633d6290251bc1b739069bb0dcbb4bb5bf0d59
Alexandros Marinos <123497074+alexmarinos87@users.noreply.github.com>
2025-08-07 22:30:33 +0100

    Time: 0 ms (100%), Space: 12.6 MB (32.12%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        Reverse the order of words in the input string.
+
+        Strategy:
+        - Use split() to separate words, which automatically removes extra spaces.
+        - Reverse the list of words.
+        - Join the words with a single space.
+
+        Parameters
+        ----------
+        s : str
+            Input string with words and spaces.
+
+        Returns
+        -------
+        str
+            Words in reversed order, separated by single spaces, with no extra spaces.
+        """
+        # Split by whitespace, reverse the list, and join by single space
+        return " ".join(s.split()[::-1])
