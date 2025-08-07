f13329ea3f571a8851dae4cc04a9f103a6f9fac4
Alexandros Marinos <123497074+alexmarinos87@users.noreply.github.com>
2025-08-07 22:29:52 +0100

    Time: 2 ms (40.1%), Space: 12.4 MB (59.16%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        Find the longest common prefix among a list of strings.
+
+        Strategy:
+        - Compare characters of all strings one by one (column by column).
+        - Stop at the first mismatch or at the end of any string.
+
+        Parameters
+        ----------
+        strs : List[str]
+            List of lowercase English strings.
+
+        Returns
+        -------
+        str
+            The longest common prefix, or "" if none exists.
+        """
+        if not strs:
+            return ""
+        
+        # Take the first string as a reference
+        for i in range(len(strs[0])):
+            char = strs[0][i]
+            for s in strs[1:]:
+                # If index out of bounds or mismatch, return prefix up to i
+                if i >= len(s) or s[i] != char:
+                    return strs[0][:i]
+        return strs[0]
