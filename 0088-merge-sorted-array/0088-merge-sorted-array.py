0f5f29d14cf0620e128b6950068fda6d8eb95753
Alexandros Marinos <123497074+alexmarinos87@users.noreply.github.com>
2025-08-17 12:37:07 +0200

    Time: 0 ms (100%), Space: 12.4 MB (52.86%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -5,18 +5,19 @@ class Solution(object):
         :type m: int
         :type nums2: List[int]
         :type n: int
-        :rtype: None Do not return anything, modify nums1 in-place instead.
+        :rtype: None  (modify nums1 in-place)
+
         Idea:
-            - Two pointers from the end and one right index
-            - Always place the larger of nums1[i] and nums[j] at nums1[w]
-            - Filter from right to avoid overwriting unread values
+          - Two pointers from the end and one write index.
+          - Always place the larger of nums1[i] and nums2[j] at nums1[w].
+          - Fill from the right to avoid overwriting unread values.
 
         Correctness invariant:
-            - After each iteration elements from the right of w are final and sorted.
+          - After each iteration, elements to the right of w are final and sorted.
 
-        Time: O(m+n) Space: O(1)
+        Time: O(m + n),  Space: O(1)
         """
-        i, j, w = m - 1, n -1, m + n -1
+        i, j, w = m - 1, n - 1, m + n - 1
         while j >= 0:
             if i >= 0 and nums1[i] > nums2[j]:
                 nums1[w] = nums1[i]
