590903b492a06df7dc5d34a99c6e84bcd9b4c988
Alexandros Marinos <123497074+alexmarinos87@users.noreply.github.com>
2025-08-18 15:31:52 +0200

    Time: 18 ms (17.32%), Space: 13.7 MB (64.49%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -4,6 +4,9 @@ class Solution(object):
         :rtype: int
         """
         from collections import Counter
-        """Time O(N), Space O(U)"""
+        """
+        Time O(N), Space O(U)
+        I use Python's collections. Counter to count the frequency of each number in the array. Since the majority element is guaranteed to appear more than n//2 times, I can use most_common(1) to directly return the element with the highest count. This solution runs in O(n) time because it makes one pass through the list to build the counter. However, it uses O(u) space, where u is the number of unique elements in the list, so it does not satisfy the follow-up constraint of O(1) space.
+        """
         def majorityElement(self, nums):
             return Counter(nums).most_common(1)[0][0]
