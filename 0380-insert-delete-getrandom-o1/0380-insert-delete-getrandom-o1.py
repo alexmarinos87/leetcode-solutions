731d7e7e44fc65541edd0d1898a769f125b09761
Alexandros Marinos <123497074+alexmarinos87@users.noreply.github.com>
2025-08-19 09:34:11 +0200

    Time: 229 ms (53.01%), Space: 55.7 MB (21.3%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -1,3 +1,5 @@
+import random
+
 class RandomizedSet(object):
 
     def __init__(self):
@@ -5,53 +7,45 @@ class RandomizedSet(object):
         Use a list for values and a hash map to track indices.
         Time: O(1) avg per operation
         """
-        self.indices = {}
-        self.nums = []
-        
+        self.indices = {}  # value -> index in self.nums
+        self.nums = []     # list of current values
 
     def insert(self, val):
         """
-        :type val: int
-        :rtype: bool
         Add val if not present.
-        Time: O(1)
+        Returns True if inserted, False if it already exists.
+        Time: O(1) average
         """
         if val in self.indices:
             return False
-        self.indices[val] = len(self.nums)
+        self.indices[val] = len(self.nums)  # index where it'll be appended
         self.nums.append(val)
         return True
 
     def remove(self, val):
         """
-        :type val: int
-        :rtype: bool
-        Remove val if present
-        Time: O(1)
+        Remove val if present.
+        Returns True if removed, False if it didn't exist.
+        Time: O(1) average
         """
         if val not in self.indices:
             return False
-        idx = self.indices[val]
-        last = self.nums[-1]
+
+        idx = self.indices[val]     # where val sits in the list
+        last = self.nums[-1]        # last element in the list
+
+        # Move the last element to fill the gap at idx
         self.nums[idx] = last
-        self.indices [last] = idx
+        self.indices[last] = idx
+
+        # Remove the last slot
         self.nums.pop()
         del self.indices[val]
         return True
-        
 
     def getRandom(self):
         """
-        :rtype: int
         Return a random element.
         Time: O(1)
         """
         return random.choice(self.nums)
-        
-
-
-# Your RandomizedSet object will be instantiated and called as such:
-# obj = RandomizedSet()
-# param_1 = obj.insert(val)
-# param_2 = obj.remove(val)
-# param_3 = obj.getRandom()
