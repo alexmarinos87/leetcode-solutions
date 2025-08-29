00f45c01fd7f95e5a293d32c7f7eda392f1bf75e
Alexandros Marinos <123497074+alexmarinos87@users.noreply.github.com>
2025-08-29 20:35:48 +0000

    [LeetCode Sync] Runtime - 1197 ms (54.11%), Memory - 0.0B (100.00%)

--- 1509-replace-employee-id-with-the-unique-identifier/solution.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/solution.sql
@@ -1,7 +1,7 @@
+# Write your MySQL query statement below
 SELECT
     uni.unique_id,
     e.name
 FROM Employees AS e
 LEFT JOIN EmployeeUNI AS uni
-ON e.id = uni.id
-ORDER BY uni.unique_id DESC;
+ON e.id = uni.id;
