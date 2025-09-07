c468d8091462c3ccb68a1464f72ac04dfd2bec09
Alexandros Marinos <123497074+alexmarinos87@users.noreply.github.com>
2025-09-07 17:33:33 +0000

    [LeetCode Sync] Runtime - 598 ms (86.84%), Memory - 0.0B (100.00%)

--- 2087-confirmation-rate/README.md
+++ 2087-confirmation-rate/README.md
@@ -1,4 +1,4 @@
-<h2><a href="https://leetcode.com/problems/confirmation-rate">2087. Confirmation Rate</a></h2><h3>Medium</h3><hr><p>Table: <code>Signups</code></p>
+<p>Table: <code>Signups</code></p>
 
 <pre>
 +----------------+----------+
--- 2087-confirmation-rate/solution.sql
+++ 2087-confirmation-rate/solution.sql
@@ -1,18 +1,11 @@
+# Write your MySQL query statement below
 SELECT
     s.user_id,
-    -- count how many “confirmed” rows,
-    -- divide by total rows, null‐safe, default to 0
-    ROUND(COALESCE(
-      SUM(CASE WHEN c.action = 'confirmed' THEN 1 ELSE 0 END)
-      * 1.0
-      / NULLIF(COUNT(c.action), 0)
-    , 0),2) AS confirmation_rate
-FROM
-    Signups AS s
-LEFT JOIN
-    Confirmations AS c
-  ON
-    s.user_id = c.user_id
-GROUP BY
-    s.user_id;
-
+    ROUND(CASE WHEN COUNT(DISTINCT c.time_stamp) = 0 THEN 0 
+    ELSE COUNT(DISTINCT conf.time_stamp)/ COUNT(DISTINCT c.time_stamp) END ,2) AS confirmation_rate
+FROM Signups AS s
+LEFT JOIN Confirmations AS c
+ON s.user_id = c.user_id
+LEFT JOIN (SELECT user_id, time_stamp, action FROM Confirmations WHERE action = "confirmed") AS conf
+ON conf.user_id = s.user_id
+GROUP BY s.user_id;
