78f180b93c1717312cf5573da69a43e154cd9255
Alexandros Marinos <123497074+alexmarinos87@users.noreply.github.com>
2025-09-10 20:44:21 +0000

    [LeetCode Sync] Runtime - 316 ms (96.28%), Memory - 0.0B (100.00%)

--- 1338-queries-quality-and-percentage/solution.sql
+++ 1338-queries-quality-and-percentage/solution.sql
@@ -1,23 +1,7 @@
-WITH
-  q AS (
-    SELECT
-      query_name,
-      AVG(rating * 1.0 / position) AS avg_ratio
-    FROM Queries
-    GROUP BY query_name
-  ),
-  pqp AS (
-    SELECT
-      query_name,
-      SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100.0 / COUNT(*) AS pct_poor
-    FROM Queries
-    GROUP BY query_name
-  )
 SELECT
-  q.query_name,
-  ROUND(q.avg_ratio, 2)           AS quality,
-  ROUND(pqp.pct_poor,   2)        AS poor_query_percentage
-FROM q
-JOIN pqp USING (query_name);
-
+    query_name,
+    ROUND(AVG(rating/position),2) AS quality,
+    ROUND(AVG(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100, 2) AS poor_query_percentage
+FROM Queries
+GROUP BY query_name;
 
