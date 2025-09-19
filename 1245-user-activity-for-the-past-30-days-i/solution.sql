29c4e43f1f69242900eda8c1718f8935a7528688
Alexandros Marinos <123497074+alexmarinos87@users.noreply.github.com>
2025-09-19 20:01:03 +0000

    [LeetCode Sync] Runtime - 437 ms (93.17%), Memory - 0.0B (100.00%)

--- 1245-user-activity-for-the-past-30-days-i/solution.sql
+++ 1245-user-activity-for-the-past-30-days-i/solution.sql
@@ -1,9 +1,9 @@
 SELECT
-  activity_date AS `day`,
+  activity_date AS day,
   COUNT(DISTINCT user_id) AS active_users
 FROM Activity
-WHERE 
-    activity_date > DATE_SUB('2019-07-27', INTERVAL 30 DAY)
-    AND activity_date <= '2019-07-27'
-GROUP BY
-  activity_date;
+WHERE activity_date >= DATE_SUB('2019-07-27', INTERVAL 29 DAY)
+  AND activity_date <= '2019-07-27'
+GROUP BY activity_date
+ORDER BY day;
+
